5b4788882db0d99699a95a604afab2da48ae70ca
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2023-09-24 12:00:16 +0900

    Add: ルーティング調整とログイン制限処理を実装しました
    - routes.rb: ルートページ（'/'）アクセスすると、Books一覧が表示される
    - application_controller.rb: 未ログイン時にアクセスすると、ログインページへリダイレクトする

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'books#index'
+  get '/books', to: redirect('/')
+
   devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  root to: 'home#index'
 end
