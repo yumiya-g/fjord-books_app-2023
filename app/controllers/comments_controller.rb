54f26972618061b2fbbdcabe40baf06dc2986b3c
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2023-12-12 22:54:45 +0900

    Fix: `destroy`アクションを実装しました
    - インスタンス変数@commentableへの代入を無くし、`set_commentable`メソッドの戻り値を使用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_commentable, only: %i[create edit update destroy]
+  before_action :set_commentable, only: %i[create edit update]
+  before_action :delete_comment, only: %i[destroy]
 
   def create
     comment = Comment.new(
       content: comment_params[:content],
       user_id: current_user.id,
-      commentable_type: @commentable.class.name,
-      commentable_id: @commentable[:id]
+      commentable_type: set_commentable.class.name,
+      commentable_id: set_commentable[:id]
     )
 
     if comment.save
-      redirect_to @commentable, notice: 'コメントしました'
+      redirect_to set_commentable, notice: 'コメントしました'
     else
-      redirect_to @commentable, alert: 'コメントできませんでした'
+      redirect_to set_commentable, alert: 'コメントできませんでした'
     end
   end
 
@@ -22,16 +23,26 @@ class CommentsController < ApplicationController
 
   def update; end
 
-  def destroy; end
+  def destroy
+    if delete_comment.destroy
+      redirect_to request.referer, notice: 'コメントを削除しました'
+    else
+      redirect_to request.referer, alert: 'コメントを削除できませんでした'
+    end
+  end
 
   private
 
   def set_commentable
-    @commentable = if params[:report_id]
-                     Report.find(params[:report_id])
-                   else
-                     Book.find(params[:book_id])
-                   end
+    if params[:report_id]
+      Report.find(params[:report_id])
+    else
+      Book.find(params[:book_id])
+    end
+  end
+
+  def delete_comment
+    Comment.find(params[:id])
   end
 
   def comment_params
