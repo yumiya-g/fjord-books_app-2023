44e8b8f4602f29269ba8973c01de2798df8552b3
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2023-12-25 00:00:46 +0900

    Fix： 日報一覧にgem「kaminari」でページネーションを実装しました
    - ページネーションリンクを追加
    - 本やユーザーと同様に、カードレイアウトで日報を表示

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
 
   # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.all.order(:id).page(params[:page])
   end
 
   # GET /reports/1 or /reports/1.json
