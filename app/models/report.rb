69d479b8f47415cc942efb9a6f534f9541455f62
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-01-05 14:02:04 +0900

    Fix: ユーザー名またはユーザーメールアドレスの表示条件を調整しました
    - Reportモデルに`account_name`メソッドを定義し、`report.html.erb`で呼び出す
    - 不要になった`before_action`を削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,3 +4,7 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 end
+
+def account_name(report_owner_id)
+  @account_name = User.find(report_owner_id).name.presence || User.find(report_owner_id).email
+end
