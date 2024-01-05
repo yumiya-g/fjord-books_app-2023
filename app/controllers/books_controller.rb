1bc0c7cd8ea530381f0846d79cfad0268e80cbdb
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-01-05 12:58:10 +0900

    Delete: 不要なjsonレスポンスの削除を行いました
    - 既存のBookモデルからもjsonレスポンスを削除しました

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,10 +29,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_create', name: Book.model_name.human) }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -42,10 +40,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: t('controllers.common.notice_update', name: Book.model_name.human) }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,7 +52,6 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -35,10 +35,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.save
         format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-        format.json { render :show, status: :created, location: @report }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -48,10 +46,8 @@ class ReportsController < ApplicationController
     respond_to do |format|
       if @report.update(report_params)
         format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-        format.json { render :show, status: :ok, location: @report }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -63,7 +59,6 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-      format.json { head :no_content }
     end
   end
 
