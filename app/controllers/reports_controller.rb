98eff3264e2654d8a6504b6129b4381280853629
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-01-07 14:37:31 +0900

    Fix: フォーマットがHTMLのみなので、espond_toメソッドを削除しました
     - 他ユーザーのコメントを編集した際に表示されるエラーメッセージも削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -31,33 +31,26 @@ class ReportsController < ApplicationController
       user_id: current_user.id
     )
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
-
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
@@ -71,7 +64,6 @@ class ReportsController < ApplicationController
     @report = current_user.reports.find(params[:id])
   rescue StandardError
     redirect_to action: :index
-    flash[:alert] = '他のユーザーが作成した日報は編集できません'
   end
 
   # Only allow a list of trusted parameters through.
