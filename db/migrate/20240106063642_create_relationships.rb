2c261ce5ca3a1eee7e5c142f4e3a3ff60948dc94
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-01-13 13:32:49 +0900

    Fix: `mentioning_report_id`カラムと`mentioned_report`カラムに、ユニーク制約を行いました
    - `:name`オプションを付与して、別名を指定

--- db/migrate/20240106063642_create_relationships.rb
+++ db/migrate/20240106063642_create_relationships.rb
@@ -3,7 +3,7 @@ class CreateRelationships < ActiveRecord::Migration[7.0]
     create_table :relationships do |t|
       t.references :mentioning_report, null: false, foreign_key: {to_table: :reports}
       t.references :mentioned_report, null: false, foreign_key: {to_table: :reports}
-      t.index [ :mentioning_report, :mentioned_report ], unique: true
+      t.index [ :mentioning_report_id, :mentioned_report_id ], name: 'index_relationships_mentioning_report_mentioned_report' , unique: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_06_063642) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_06_063642) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -64,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_06_063642) do
     t.integer "mentioned_report_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "\"mentioning_report\", \"mentioned_report\"", name: "index_relationships_on_mentioning_report_and_mentioned_report", unique: true
     t.index ["mentioned_report_id"], name: "index_relationships_on_mentioned_report_id"
+    t.index ["mentioning_report_id", "mentioned_report_id"], name: "index_relationships_mentioning_report_mentioned_report", unique: true
     t.index ["mentioning_report_id"], name: "index_relationships_on_mentioning_report_id"
   end
 
