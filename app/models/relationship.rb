63fe7a76d37dbf744f9ee2ad3fb1941828dfcd49
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-01-15 22:04:39 +0900

    Fix: ReportモデルとRelationshipモデルに双方向関連付けを行いました

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Relationship < ApplicationRecord
-  belongs_to :mentioning_report, class_name: 'Report', foreign_key: :mentioning_report_id
-  belongs_to :mentioned_report, class_name: 'Report', foreign_key: :mentioned_report_id
+  belongs_to :mentioning_report, class_name: 'Report'
+  belongs_to :mentioned_report, class_name: 'Report'
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,25 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-	has_many :reports_mentioned, class_name: 'Relationship', foreign_key: :mentioned_report_id
-	has_many :mentioning_reports, through: :reports_mentioned, source: :mentioned_report
-	has_many :reports_mentioning, class_name: 'Relationship', foreign_key: :mentioning_report_id
-  has_many :mentioned_reports, through: :reports_mentioning, source: :mentioning_report
+  has_many :reports_mentioning,
+           class_name: 'Relationship',
+           foreign_key: :mentioning_report_id,
+           dependent: :destroy,
+           inverse_of: :mentioning_report
+
+  has_many :mentioning_reports,
+           through: :reports_mentioning,
+           source: :mentioned_report
+
+  has_many :reports_mentioned,
+           class_name: 'Relationship',
+           foreign_key: :mentioned_report_id,
+           dependent: :destroy,
+           inverse_of: :mentioned_report
+
+  has_many :mentioned_reports,
+           through: :reports_mentioned,
+           source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
