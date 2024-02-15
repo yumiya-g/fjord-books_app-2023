faa8db7a07481376fd85f5703b4b6f64f2a52640
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-02-15 22:38:43 +0900

    Fix: 日報編集でバリデーションエラー時の表示処理を修正しました
    - `render :new`を`render :edit`に修正しました

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,7 +30,7 @@ class ReportsController < ApplicationController
     if @report.update_with_mention(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
